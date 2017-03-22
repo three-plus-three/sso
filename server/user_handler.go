cb9710e7f92bd27010a9ed389bfda4dd3efea2df
runner.mei <runner.mei@gmail.com>
2017-03-22 17:07:24 +0800

    add time format for postgres

--- server/user_handler.go
+++ server/user_handler.go
@@ -431,7 +431,9 @@ func parseTime(layout, s string) time.Time {
 		}
 	}
 
-	for _, layout := range []string{time.RFC3339Nano, time.RFC3339} {
+	for _, layout := range []string{time.RFC3339Nano,
+		time.RFC3339,
+		"2006-01-02 15:04:05.999999999Z07:00"} {
 		t, err := time.Parse(layout, s)
 		if err == nil {
 			return t
