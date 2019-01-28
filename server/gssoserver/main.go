9ee0f920f5b26624fcda577c9ac900601e9d7fc2
meifakun <runner.mei@gmail.com>
2019-01-28 13:37:04 +0800

    fix bugs

--- server/gssoserver/main.go
+++ server/gssoserver/main.go
@@ -13,18 +13,14 @@ func main() {
 		UserConfig: dbConfig,
 		//UrlPrefix: "hw",
 		HeaderTitleText: "单点登录系统 v1.0",
-		FooterTitleText: "© 2017 恒维信息技术(上海)有限公司, 保留所有版权。",
+		FooterTitleText: "© 2019 ",
 		TicketProtocol:  "jwt",
 		TicketConfig:    map[string]interface{}{},
 	}
 
 	flag.StringVar(&config.ListenAt, "listen", ":9031", "")
 	flag.StringVar(&dbConfig.DbType, "dbType", "postgres", "")
-	flag.StringVar(&dbConfig.Address, "dbHost", "127.0.0.1", "")
-	flag.StringVar(&dbConfig.Port, "dbPort", "5432", "")
-	flag.StringVar(&dbConfig.DbName, "dbName", "tpt_models", "")
-	flag.StringVar(&dbConfig.Username, "dbUsername", "tpt", "")
-	flag.StringVar(&dbConfig.Password, "dbPassword", "extreme", "")
+	flag.StringVar(&dbConfig.DbURL, "dbURL", "host=127.0.0.1 dbname=tpt_models_test user=postgres password=123456 sslmode=disable", "")
 
 	flag.Parse()
 	if nil != flag.Args() && 0 != len(flag.Args()) {
